20054adb1d043c49059fd73e40ef61a16b43561a
Jirat Ki <saakyz@gmail.com>
2020-03-11 13:05:26 +0800

    Add JSX extension to @rollup/plugin-node-resolve options (#524)
    * Add JSX extension to @rollup/plugin-node-resolve options
    
    * Add test for JSX chaining import
    
    * Correct comment link

--- src/createRollupConfig.ts
+++ src/createRollupConfig.ts
@@ -118,6 +118,8 @@ export async function createRollupConfig(
           'main',
           opts.target !== 'node' ? 'browser' : undefined,
         ].filter(Boolean) as string[],
+        // defaults + .jsx
+        extensions: ['.mjs', '.js', '.jsx', '.json', '.node'],
       }),
       opts.format === 'umd' &&
         commonjs({
